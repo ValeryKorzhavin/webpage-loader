c5c829cdb22bcbb16c4d63dbdee7d4fa74616e73
Valery Korzhavin <white.and.bushy@gmail.com>
2019-05-02 15:43:33 +0300

    fix lint errors

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -1,11 +1,10 @@
-import nock from 'nock';
-import { promises as fs } from 'fs';
-import path from 'path';
-import pageLoader from '../src';
-import axios from 'axios';
-import httpAdapter from 'axios/lib/adapters/http';
 import os from 'os';
-import url from 'url';
+import path from 'path';
+import nock from 'nock';
+import axios from 'axios';
+import { promises as fs } from 'fs';
+import httpAdapter from 'axios/lib/adapters/http';
+import pageLoader from '../src';
 
 const host = 'http://localhost';
 
@@ -14,18 +13,15 @@ const fixtures = path.join(__dirname, '__fixtures__');
 axios.default.host = host;
 axios.default.adapter = httpAdapter;
 
-
 describe('suit', () => {
   it('test', async () => {
-
     const testData = await fs.readFile(path.join(fixtures, 'sample_page.html'), 'utf-8');
     nock(host)
       .get('/test')
       .reply(200, testData);
 
-
-    const address = host + '/test';
-    const output = await fs.mkdtemp(path.join(os.tmpdir(), 'out-')); 
+    const address = `${host}/test`;
+    const output = await fs.mkdtemp(path.join(os.tmpdir(), 'out-'));
     await pageLoader(address, output);
   });
 });
--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import url from 'url';
 
-export default (page, output) => {  
-
-  const protocol = url.parse(page).protocol;
-  const outputFileName = page.slice((protocol + '//').length).replace(/\W/g, '-') + '.html';
-
-  return axios.get(page).then(result => fs.writeFile(path.join(output, outputFileName), result.data));
-
+export default (page, output) => {
+  const { protocol } = url.parse(page);
+  const outputFileName = `${page.slice((`${protocol}//`).length).replace(/\W/g, '-')}.html`;
+  return axios
+    .get(page)
+    .then(result => fs.writeFile(path.join(output, outputFileName), result.data));
 };
